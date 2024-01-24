f4aade47eec077990cebfe11e75085ff76c77cf2
deividepaulino1 <deividepcsvlog@gmail.com>
2024-01-24 20:12:40 -0300

    🚀  feat: Criado o método de exemplo para auxilio ao lucas

--- lib/login_module/view/reducer/login_reducer.dart
+++ lib/login_module/view/reducer/login_reducer.dart
@@ -1,11 +1,18 @@
+import 'package:anty_front/login_module/view/atoms/login_atoms.dart';
 import 'package:asp/asp.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 class LoginReducer extends Reducer {
   //necessario instancia do atomo
+  final loginAtoms = Modular.get<LoginAtoms>();
 
   LoginReducer() {
     /// on(() => [atom aqui], _metodoAqui);
+
+    on(() => [loginAtoms.doLogin], _doLogin);
   }
 
   // void _metodoAqui(){}
+
+  void _doLogin() async {}
 }
