afff018f4c5a799333e3a984ad315ffbc887f2f4
deividepaulino1 <deividepcsvlog@gmail.com>
2024-01-24 20:16:15 -0300

    🚀  feat: Criado exemplo de tema para o lucas configurar

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,3 +1,4 @@
+import 'package:anty_front/core/themes/main_theme.dart';
 import 'package:asp/asp.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
@@ -17,6 +18,7 @@ class _AppWidgetState extends State<AppWidget> {
         debugShowCheckedModeBanner: false,
         title: 'anty',
         themeMode: ThemeMode.light,
+        theme: ThemeAnt.antWhite,
         routeInformationParser: Modular.routeInformationParser,
         routerDelegate: Modular.routerDelegate,
       ),
---
+++ lib/core/themes/main_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ThemeAnt {
+  static ThemeData get antWhite {
+    return ThemeData(
+      primarySwatch: Colors.deepOrange,
+      primaryColor: Colors.deepOrange,
+      primaryColorLight: Colors.deepOrange.shade200,
+    );
+  }
+}
